711fd9f95faf1764c6ad6aaa0b45e30f0c27fa47
hydtrng <hydtrng@SONATA.LOCAL>
2022-08-23 15:25:07 +0530

    python files

--- pythonProject/venv/rpsfun.py
+++ pythonProject/venv/rpsfun.py
@@ -1,7 +1,7 @@
 def r_p_s(p1,p2):
     if(p1=='r' and p2=='p') or (p1=='p' and p2=='s') or (p1=='s' and p2=='r'):
         return p2
-    elif(p1=="p" and p2=="r") or (p1=='s' and p2=='p') or (p1=='r' and p2=='s'):
+    elif(p1=='p' and p2=='r') or (p1=='s' and p2=='p') or (p1=='r' and p2=='s'):
         return p1
     else:
         return 'nothing wins,match was tie'
